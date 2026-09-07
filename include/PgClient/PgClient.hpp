e81295011bc15aef3f80c6f4878d8165707aaf88
HANABLI <nablihatem3@gmail.com>
2026-09-07 12:12:47 +0200

    fix: if the distructor is declared as noexcept so it should be implemented with the same operator.

--- include/PgClient/PgClient.hpp
+++ include/PgClient/PgClient.hpp
@@ -23,7 +23,7 @@ namespace Postgresql
     {  // Life cycle managment
 
     public:
-        ~PgClient();
+        ~PgClient() noexcept;
         PgClient(const PgClient&) = delete;
         PgClient(PgClient&&) noexcept = default;
         PgClient& operator=(const PgClient&) = delete;
